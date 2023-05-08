9d2659a4de3d602201ea11701c66c419c542abd6
Ylkaa <ylkakolken@hotmail.com>
2023-05-08 14:30:41 +0200

    added geomean calculation

--- extracting_from_raw_data.py
+++ extracting_from_raw_data.py
@@ -10,17 +10,24 @@ BASEDIR_NODELAY = dotenv_values('.env')['BASEDIR_NODELAY']
 BASEDIR_DELAY = dotenv_values('.env')['BASEDIR_DELAY']
 CSV_STOREDIR = dotenv_values('.env')['CSV_STOREDIR']
 
+group = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
+oriincrement = [0.2, 0.4, 0.6, 0.8, 1.0, 1.20, 1.42, 1.68, 2.02, 2.42, 2.91, 3.49, 4.19, 5.02, 6.03, 7.23, 8.68, 10.4,
+                12.5, 15.0, 18.0, 21.6, 25.9, 31.1, 37.3, 44.8]
+translateOriincrement = pd.DataFrame({'oriincrementid': group, 'oriincrement': oriincrement})
+
 def extract_nodelay_data(infile, dataframe):
     with open(infile, 'r') as f:
         data = f.read().strip().split('\n')
         cnd = None
         filename = None
+        case = None
+        jnd = None
         for row in data:
             # using regular expression to extract data from strings
             condition_regex1 = re.search(r"\s+Sav\sfile\s:\s(\S+)\.?s?a?v?\s+c?o?a?n?d?i?\s?\s?\.?(\d+)", row)
             condition_regex2 = re.search(r"Datafile\s:.+co?ndi?\s*?(\d+)", row)
             savfile_regex = re.search(r"\s+Sav\sfile\s:\s(\S+)\.sav", row)
-            score_regex = re.search(r"\s+NS\s:\s+(\d).+JND\s:\s(\d+.\d?\d?)", row)
+            score_regex = re.search(r"\s+?NS\s:\s+(\d).+JND\s:\s(\d+.\d?\d?)", row)
             if condition_regex1 is not None:
                 filename, cnd = condition_regex1.groups()
             elif condition_regex2 is not None:
@@ -29,6 +36,13 @@ def extract_nodelay_data(infile, dataframe):
                 filename = savfile_regex.groups()[0]
             elif score_regex is not None:
                 case, jnd = score_regex.groups()
+            elif row.strip().startswith('Rps: '):
+                df = pd.DataFrame([x for x in re.sub(' +', ' ', row.strip().removeprefix('Rps:').strip()).split(' ')],
+                                  columns=['oriincrementid']).astype('int')
+                df = df.merge(translateOriincrement, on='oriincrementid', how='left')
+                df['logn'] = df['oriincrement'].apply(float).apply(np.log)
+                value = math.exp(df.logn.iloc[-10:].mean())
+                points = df.logn.iloc[-10:].count()
                 dataframe = pd.concat([dataframe, pd.DataFrame({
                     'Participant': participant.replace('pp', ''),
                     'Session': file.split('.')[-1],
@@ -38,9 +52,9 @@ def extract_nodelay_data(infile, dataframe):
                     'Condition': cnd,
                     'Staircase': case,
                     'Score': jnd,
+                    'Value': value,
+                    'Points': points,
                     'Delay/No delay': 'No delay'}, index=[0])])
-            else:
-                next
     return dataframe
 
 
@@ -67,22 +81,21 @@ def extract_delay_data(infile, dataframe):
             condition_regex1 = re.search(r'.*geometric=(\d+.\d*),*', row)
             if condition_regex1 is not None:
                 cnd = condition_regex1.groups()
-                dataframe = pd.concat([dataframe, pd.DataFrame({
-                    'Participant': pp,
-                    'Session': s,
-                    'Condition': c,
-                    'Staircase': r,
-                    'Score': cnd,
-                    'Value': value,
-                    'Source': file,
-                    'Delay/No delay': 'Delay'}, index=[0])])
-            else:
-                next
+        dataframe = pd.concat([dataframe, pd.DataFrame({
+             'Participant': pp,
+             'Session': s,
+             'Condition': c,
+             'Staircase': r,
+             'Score': cnd,
+             'Value': value,
+             'Source': file,
+             'Delay/No delay': 'Delay'}, index=[0])])
     return dataframe
 
 
-table = pd.DataFrame(columns=['Participant', 'Session', 'Source', 'File', 'Condition', 'Staircase', 'Score', 'Value'])
-
+table = pd.DataFrame(columns=['Participant', 'Session', 'Source',
+                              'File', 'Condition', 'Staircase',
+                              'Score', 'Value', 'Points'])
 # NO DELAY
 for participant in os.listdir(BASEDIR_NODELAY):
     if not participant.startswith('.'):
@@ -106,8 +119,8 @@ for participant in os.listdir(BASEDIR_DELAY):
                                 table = extract_delay_data(
                                     os.path.join(BASEDIR_DELAY, participant, part, session, file), table)
 
-experiment = pd.DataFrame({'Condition': ['15','24', '24', '16'],
-                           'Delay/No delay': ['Delay', 'Delay', 'No delay', 'No delay'],
+experiment = pd.DataFrame({'Condition': ['12','12', '16', '16'],
+                           'Delay/No delay': ['Delay', 'No delay', 'Delay', 'No delay'],
                            'Experiment': ['3', '3', '3', '3']})
 
 early_late = pd.DataFrame({'Session': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'],
@@ -116,7 +129,7 @@ early_late = pd.DataFrame({'Session': ['1', '2', '3', '4', '5', '6', '7', '8', '
 table = table.merge(experiment, on=['Condition', 'Delay/No delay'], how='right')
 table = table.merge(early_late, on='Session', how='right')
 
-table.to_csv(os.path.join(CSV_STOREDIR, 'EXTRACTED_H5.csv'), index=False)
+table.to_csv(os.path.join(CSV_STOREDIR, 'EXTRACTED_ACA_test.csv'), index=False)
 
 print(table)
 
